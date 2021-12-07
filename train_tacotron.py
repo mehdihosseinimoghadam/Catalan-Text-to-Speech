d4d9f5ac71eda67ae5f4b98c142f80ab89c285b4
cschaefer26 <c.schaefer.home@gmail.com>
2021-12-07 14:41:03 +0100

    pitch pred

--- train_tacotron.py
+++ train_tacotron.py
@@ -66,11 +66,11 @@ def extract_pitch_energy(save_path_pitch: Path,
     for item_id, phoneme_energy in phoneme_energies:
         np.save(str(save_path_energy / f'{item_id}.npy'), phoneme_energy, allow_pickle=False)
 
-    mean, var = normalize_values(phoneme_pitches)
+    for i in range(len(phoneme_pitches)):
+        mean, var = normalize_values(phoneme_pitches[i])
+        print(f'\n{i} Pitch mean: {mean} var: {var}')
     for item_id, phoneme_pitch in phoneme_pitches:
         np.save(str(save_path_pitch / f'{item_id}.npy'), phoneme_pitch, allow_pickle=False)
-
-    print(f'\nPitch mean: {mean} var: {var}')
 
     return mean, var
 

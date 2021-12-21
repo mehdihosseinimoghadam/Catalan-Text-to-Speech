a0eb1be9c58b7cef23ced26c960607f28ec6a25a
cschaefer26 <c.schaefer.home@gmail.com>
2021-12-21 15:39:44 +0100

    Testing.

--- train_tacotron.py
+++ train_tacotron.py
@@ -136,8 +136,11 @@ def create_align_features(model: Tacotron,
         seq, att, mel_len, item_id = batch['x'][0], att_batch[0], batch['mel_len'][0], batch['item_id'][0]
         att_rev = att_batch_rev[0]
         align_score, sharp_score = float(align_score[0]), float(sharp_score[0])
+
         att_score_dict[item_id] = (align_score, sharp_score)
         durs = dur_extraction_func(seq, att, mel_len)
+        print(sharp_score)
+        print(durs)
         if np.sum(durs) != mel_len:
             print(f'WARNINNG: Sum of durations did not match mel length for item {item_id}!')
         np.save(str(paths.alg / f'{item_id}.npy'), durs, allow_pickle=False)

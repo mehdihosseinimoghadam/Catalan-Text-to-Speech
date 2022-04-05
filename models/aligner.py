0da4e3acc770a19671e85e3940cd7695f1b7d7a0
cschafer26 <c.schaefer.home@gmail.com>
2022-04-05 13:21:13 +0200

    According to orig paper.

--- models/aligner.py
+++ models/aligner.py
@@ -15,29 +15,17 @@ class Aligner(nn.Module):
     def __init__(self, num_chars):
         super().__init__()
         self.register_buffer('step', torch.zeros(1, dtype=torch.long))
-        self.embedding = Embedding(num_embeddings=num_chars, embedding_dim=256)
+        self.embedding = Embedding(num_embeddings=num_chars, embedding_dim=512)
 
         self.text_encoder = nn.Sequential(
-            nn.Conv1d(in_channels=256, out_channels=256, kernel_size=5, padding=2),
-            nn.BatchNorm1d(256),
-            nn.Dropout(0.1),
-            nn.Conv1d(in_channels=256, out_channels=256, kernel_size=5, padding=2),
-            nn.BatchNorm1d(256),
-            nn.Dropout(0.1),
-            nn.Conv1d(in_channels=256, out_channels=64, kernel_size=5, padding=2)
+            nn.Conv1d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
+            nn.Conv1d(in_channels=512, out_channels=512, kernel_size=1, padding=1)
         )
         self.mel_encoder = nn.Sequential(
-            nn.Conv1d(in_channels=80, out_channels=256, kernel_size=5, padding=2),
-            nn.BatchNorm1d(256),
-            nn.Dropout(0.1),
-            nn.Conv1d(in_channels=256, out_channels=256, kernel_size=5, padding=2),
-            nn.BatchNorm1d(256),
-            nn.Dropout(0.1),
-            nn.Conv1d(in_channels=256, out_channels=64, kernel_size=5, padding=2)
+            nn.Conv1d(in_channels=80, out_channels=512, kernel_size=3, padding=1),
+            nn.Conv1d(in_channels=512, out_channels=512, kernel_size=5, padding=1),
+            nn.Conv1d(in_channels=512, out_channels=512, kernel_size=5, padding=1)
         )
-
-        self.gru_1 = GRU(64, 64, bidirectional=True)
-        self.gru_2 = GRU(64, 64, bidirectional=True)
 
     def forward(self, x: torch.Tensor, m: torch.Tensor) -> torch.Tensor:
         if self.training:
@@ -50,9 +38,6 @@ class Aligner(nn.Module):
 
         x = x.transpose(1, 2)
         m = m.transpose(1, 2)
-
-        x, _ = self.gru_1(x)
-        m, _ = self.gru_2(m)
 
         diff = x[:, None, :, :] - m[:, :, None, :]
         dist = -torch.linalg.norm(diff, ord=2, dim=-1)
